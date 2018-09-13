08cc6fcad2e3272a85845e676df4bc30598c538d
sarath6080 <sarathkmr05@gmail.com>
2018-09-13 11:49:26 +0530

    add new package for getting the block headers

--- findingBlck.js
+++ findingBlck.js
@@ -1,29 +1,45 @@
-// var blockexplorer = require('blockchain.info/blockexplorer').usingNetwork(3)
-// var blckdetails = blockexplorer.getLatestBlock()
-// console.log(blckdetails)
+// used with Bitcoin-core package.
+const BitcoindClient = require("bitcoin-core");
+const config =  {
+    network: 'testnet',
+    username: 'xxxxx',
+    password: 'xxxxx',
+    host: "127.0.0.1" 
+};
 
-var bitcoin = require('bitcoin');
+const bitcoindclient = new BitcoindClient(config);
+
+bitcoindclient.getBlockchainInfo().then((help) => console.log(help));
+
+bitcoindclient.getBalance('*', 6, function(err, balance, resHeaders) {
+    if (err) return console.log(err);
+    console.log('Balance:', balance);
+  });
+/*
+// used in bitcoin packaage
 var client = new bitcoin.Client({
-  host: 'localhost',
-  port: 8332,
-  user: 'ApplePro',
-  pass: 'ApplePro5'
+    host: 'localhost',
+    port: 8332,
+    user: 'xxxx',
+    pass: 'xxxx'
 });
 
 client.getDifficulty(function(err, difficulty) {
-  if (err) {
-    return console.error(err);
-  }
+    if (err) {
+        return console.error(err);
+    }
 
-  console.log('Difficulty: ' + difficulty);
+    console.log('Difficulty: ' + difficulty);
 });
 
 client.getBalance('*', 6, function(err, balance, resHeaders) {
     if (err) return console.log(err);
     console.log('Balance:', balance);
-  });
+});
 
-  client.getBlockchainInfo(function(err, information) {
+client.getBlockchainInfo(function(err, information) {
     if (err) return console.log(err);
     console.log('Into:', information);
-  });
+});
+*/
+
